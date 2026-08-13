dba24c9ae281b4d033a90231996edd873e665b1c
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-13 13:31:43 -0500

    Move testing to index.js and export to index.js

--- src/projects.js
+++ src/projects.js
@@ -49,29 +49,4 @@ class Project {
     }
 }
 
-//Testing
-const clean = new Project("Clean", "A list of things that need cleaning!");
-const powerPointProject = new Project("PowerPoint", "A deck made for our new client.")
-
-powerPointProject.deleteProject(powerPointProject);
-console.log(Project.allProjects);
-
-//Create todo items
-const cleanLitterBox = new TodoItem("Clean Litter Box", "Empty, scrub, rinse, and refill box.", "August 11 2026", "High");
-const cleanKitchenFloor = new TodoItem("Clean Kitchen Floor", "Sweep and Swiffer Wet mop the floor.", "August 11 2026", "Medium")
-const cleanBathroomFloors = new TodoItem("Clean Bathroom Floor", "Sweep and Swiffer Wet mop the floors", "August 11 2026", "Medium");
-
-//Add todos to a project
-clean.addTodoItem(cleanLitterBox);
-clean.addTodoItem(cleanKitchenFloor);
-clean.addTodoItem(cleanBathroomFloors);
-
-//Edit todos
-cleanKitchenFloor.editTodoItem("Clean Kitchen Floor", "", "August 18 2026", "High");
-
-//Mark complete/delete
-clean.markTodoComplete(cleanKitchenFloor);
-console.log(completedTodoItemsArray);
-clean.deleteTodoItem(cleanLitterBox);
-
-clean.editProject("", "Clean up!");
+export {deletedTodoItemsArray, completedTodoItemsArray, deletedProjectsArray, Project};
