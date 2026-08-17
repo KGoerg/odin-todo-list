d2c0b5cd959af9011deee54c55becbbb5fcd9614
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-17 15:09:46 -0500

    Remove default project render function
    The general project render function can be used for the default project, as well. There's no reason to have two identical functions.

--- src/sidebar-render.js
+++ src/sidebar-render.js
@@ -1,16 +1,17 @@
 import { Project } from "./projects.js";
 
-export function renderDefaultProject() {
-    const defaultProject = new Project("Default Project", "A default space to put any tasks you haven't sorted yet!");
+const projectsContainer = document.querySelector(".projects-container");
 
-    const projectsContainer = document.querySelector(".projects-container");
-    const defaultProjectContainer = document.createElement("div");
-    defaultProjectContainer.classList.add("project");
-    projectsContainer.appendChild(defaultProjectContainer);
+export function renderNewProject(project) {
+    const newProject = project;
 
-    const defaultProjectHeader = document.createElement("button");
-    defaultProjectHeader.textContent = defaultProject.title;
-    defaultProjectContainer.appendChild(defaultProjectHeader);
+    const newProjectContainer = document.createElement("div");
+    newProjectContainer.classList.add("project");
+    projectsContainer.appendChild(newProjectContainer);
+
+    const newProjectHeader = document.createElement("button");
+    newProjectHeader.textContent = newProject.title;
+    newProjectContainer.appendChild(newProjectHeader);
 }
 
 // On-click of the Default Project button, generate the project title, description below title, and todo items, in the content section. Need to figure out how to organize code in order to do this.
