447dfc565b6d0c1ea35fd5858da9eaa36b4c5033
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-18 11:37:34 -0500

    Move code related to sidebar buttons to own module

--- src/sidebar-render.js
+++ src/sidebar-render.js
@@ -2,18 +2,6 @@ import { Project } from "./projects.js";
 
 const projectsContainer = document.querySelector(".projects-container");
 
-// Uses renderNewProject() when clicking the Submit button on the form.
-const submitButton = document.querySelector('button[type="submit"]').addEventListener("click", function(event) {
-    const formTitle = document.getElementById("project_name").value;
-    const formDescription = document.getElementById("project_description").value;
-    if (formTitle === "" || formDescription === "") {
-    submitButton.disabled = true;
-  } else {
-    const project = new Project(formTitle, formDescription);
-    renderNewProject(project);
-    console.log(Project.allProjects);
-}});
-
 //Creates button with project's name
 export function renderNewProject(project) {
     const newProject = project;
@@ -36,11 +24,5 @@ export function renderNewProject(project) {
     deleteButton.textContent = "Delete";
     newProjectContainer.appendChild(deleteButton);
 };
-
-//Resets the form field after submitting
-const form = document.querySelector("form");
-form.addEventListener("submit", function() {
-  form.reset();
-});
 
 // On-click of the Default Project button, generate the project title, description below title, and todo items, in the content section. Need to figure out how to organize code in order to do this.
