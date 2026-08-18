87473335c067163257794f272ccfd40f337ed481
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-18 11:37:55 -0500

    Update imports after making sidebar buttons module

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ import "./styles.css";
 import {deletedTodoItemsArray, completedTodoItemsArray, deletedProjectsArray, Project} from "./projects.js";
 import {TodoItem} from "./todos.js";
 import {renderNewProject } from "./sidebar-render.js";
-
+import { submitButton, formReset } from "./sidebar-buttons.js";
 
 //Testing
 const defaultProject = new Project("Default Project", "A default space to put any tasks you haven't sorted yet!");
 
 renderNewProject(defaultProject);
 
-const clean = new Project("Clean", "A list of things that need cleaning!");
-const powerPointProject = new Project("PowerPoint", "A deck made for our new client.")
+// const clean = new Project("Clean", "A list of things that need cleaning!");
+// const powerPointProject = new Project("PowerPoint", "A deck made for our new client.")
 
-renderNewProject(clean);
+// renderNewProject(clean);
 
-// powerPointProject.deleteProject(powerPointProject);
-console.log(Project.allProjects);
+// // powerPointProject.deleteProject(powerPointProject);
+// console.log(Project.allProjects);
 
-// //Create todo items
-const cleanLitterBox = new TodoItem("Clean Litter Box", "Empty, scrub, rinse, and refill box.", "August 11 2026", "High");
-const cleanKitchenFloor = new TodoItem("Clean Kitchen Floor", "Sweep and Swiffer Wet mop the floor.", "August 11 2026", "Medium")
-const cleanBathroomFloors = new TodoItem("Clean Bathroom Floor", "Sweep and Swiffer Wet mop the floors", "August 11 2026", "Medium");
+// // //Create todo items
+// const cleanLitterBox = new TodoItem("Clean Litter Box", "Empty, scrub, rinse, and refill box.", "August 11 2026", "High");
+// const cleanKitchenFloor = new TodoItem("Clean Kitchen Floor", "Sweep and Swiffer Wet mop the floor.", "August 11 2026", "Medium")
+// const cleanBathroomFloors = new TodoItem("Clean Bathroom Floor", "Sweep and Swiffer Wet mop the floors", "August 11 2026", "Medium");
 
-// //Add todos to a project
-clean.addTodoItem(cleanLitterBox);
-clean.addTodoItem(cleanKitchenFloor);
-clean.addTodoItem(cleanBathroomFloors);
+// // //Add todos to a project
+// clean.addTodoItem(cleanLitterBox);
+// clean.addTodoItem(cleanKitchenFloor);
+// clean.addTodoItem(cleanBathroomFloors);
 
 // //Edit todos
 // cleanKitchenFloor.editTodoItem("Clean Kitchen Floor", "", "August 18 2026", "High");
