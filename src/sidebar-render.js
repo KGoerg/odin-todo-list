6b6a651c0aef44094a3520adfac67858c46bb44a
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-18 11:30:27 -0500

    Add code to prevent data submission if blank

--- src/sidebar-render.js
+++ src/sidebar-render.js
@@ -2,12 +2,17 @@ import { Project } from "./projects.js";
 
 const projectsContainer = document.querySelector(".projects-container");
 
+// Uses renderNewProject() when clicking the Submit button on the form.
 const submitButton = document.querySelector('button[type="submit"]').addEventListener("click", function(event) {
     const formTitle = document.getElementById("project_name").value;
     const formDescription = document.getElementById("project_description").value;
+    if (formTitle === "" || formDescription === "") {
+    submitButton.disabled = true;
+  } else {
     const project = new Project(formTitle, formDescription);
     renderNewProject(project);
-});
+    console.log(Project.allProjects);
+}});
 
 //Creates button with project's name
 export function renderNewProject(project) {
