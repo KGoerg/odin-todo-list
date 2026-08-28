c9025bef6e81022875592152ec9c36db41e7ebfa
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-28 14:41:12 -0500

    Start building DOM elements for content container

---
+++ src/content-render.js
@@ -0,0 +1,8 @@
+const contentContainer = document.querySelector("#content");
+
+export const projectTitle = document.createElement("h1");
+export const projectDescription = document.createElement("h2");
+contentContainer.appendChild(projectTitle);
+contentContainer.appendChild(projectDescription);
+
+const todosContainer = document.querySelector(".todos-container");
