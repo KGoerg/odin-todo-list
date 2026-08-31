42fd56c4edf5e9de085d3d3d410964975a5dbd08
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-08-31 14:27:39 -0500

    Add eventlistener to todo submit button
    On click, the data from the form is stored and passed into variables, which are then used on the TodoItem class to construct a new todo item.

--- src/content-render.js
+++ src/content-render.js
@@ -1,3 +1,5 @@
+import { TodoItem } from "./todos.js";
+
 const contentContainer = document.querySelector("#content");
 
 export const projectTitle = document.createElement("h1");
@@ -6,3 +8,12 @@ contentContainer.appendChild(projectTitle);
 contentContainer.appendChild(projectDescription);
 
 const todosContainer = document.querySelector(".todos-container");
+
+const todosFormSubmitButton = document.getElementById("todos-submit").addEventListener("click", function(event) {
+    const todosFormTitle = document.getElementById("todo_name").value;
+    const todosFormDescription = document.getElementById("todo_description").value;
+    const todoDueDate = document.getElementById("todo_due_date").value;
+    const todoPriorityLevel = document.getElementById("priority").value;
+    const newTodo = new TodoItem(todosFormTitle, todosFormDescription, todoDueDate, todoPriorityLevel);
+    console.log(newTodo);
+})
