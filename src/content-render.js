4d052fdb18bab85b6390842a38d9e9d68e869e56
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-09-01 12:09:56 -0500

    Create renderContent() and change todo button to DOM element
    The renderContent() function will hold everything needed to make elements for the content part of the page appear when a project's title is clicked in the sidebar. I've also set it up now so that the "New To-do Item" button is generated per project clicked, so hopefully I can tie the specific button to its specific array of to-dos, and that should solve the conundrum of figuring out how to link todos to their projects!

--- src/content-render.js
+++ src/content-render.js
@@ -16,4 +16,19 @@ const todosFormSubmitButton = document.getElementById("todos-submit").addEventLi
     const todoPriorityLevel = document.getElementById("priority").value;
     const newTodo = new TodoItem(todosFormTitle, todosFormDescription, todoDueDate, todoPriorityLevel);
     console.log(newTodo);
-})
+});
+
+const todosModal = document.querySelector("#todo-dialog");
+
+export function renderContent(project) {
+    projectTitle.textContent = project.title;
+    const newTodoButton = document.createElement("button");
+    newTodoButton.textContent = "New To-do Item";
+
+    newTodoButton.addEventListener("click", function(event) {
+        todosModal.showModal();
+    })
+    
+    contentContainer.appendChild(newTodoButton);
+    projectDescription.textContent = project.description;
+};
