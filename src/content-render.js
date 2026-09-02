d910c0c751984328fc23c3faa66b35d586c2cc15
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-09-02 13:27:43 -0500

    Rearrange code in renderContent() for readability

--- src/content-render.js
+++ src/content-render.js
@@ -4,11 +4,10 @@ const contentContainer = document.querySelector("#content");
 
 export const projectTitle = document.createElement("h1");
 export const projectDescription = document.createElement("h2");
-contentContainer.appendChild(projectTitle);
-contentContainer.appendChild(projectDescription);
 
 const todosContainer = document.querySelector(".todos-container");
 
+//Get data from the todos form and create a new todo item
 const todosFormSubmitButton = document.getElementById("todos-submit").addEventListener("click", function(event) {
     const todosFormTitle = document.getElementById("todo_name").value;
     const todosFormDescription = document.getElementById("todo_description").value;
@@ -22,17 +21,23 @@ const todosModal = document.querySelector("#todo-dialog");
 
 let currentTodoButton;
 export function renderContent(project) {
-    projectTitle.textContent = project.title;
     const newTodoButton = document.createElement("button");
     newTodoButton.textContent = "New To-do Item";
     currentTodoButton = newTodoButton;
 
+    projectTitle.textContent = project.title;
+    contentContainer.appendChild(projectTitle);
+    
+    projectDescription.textContent = project.description;
+    contentContainer.appendChild(projectDescription);
+    
+    contentContainer.appendChild(newTodoButton);
+    contentContainer.appendChild(todosContainer);
+
+    //Open todos modal on click
     newTodoButton.addEventListener("click", function(event) {
         todosModal.showModal();
     })
-
-    contentContainer.appendChild(newTodoButton);
-    projectDescription.textContent = project.description;
 };
 
 export function deleteRenderedContent(title, description, button) {
