aa6a75fb05c653403cbaba0d99e247e24fca7a0e
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-09-04 14:26:32 -0500

    Add comments for how to potentially solve delete issue
    Whenever the delete button is hit, it deletes the rendered content no matter what project is selected.

--- src/content-render.js
+++ src/content-render.js
@@ -51,10 +51,24 @@ export function renderContent(project) {
     })
 };
 
-export function deleteRenderedContent(title, description, button) {
-    title.textContent = "";
-    description.textContent = "";
-    if (currentTodoButton != null) {
+//Need to fix bug where, if a non-selected project is deleted, it still deletes what's rendered on the page. See most recent Mousepad with a plan to fix it using an ID on the todos-container.
+export function deleteRenderedContent(title, description) {
+    // if (currentProject.title === title.textContent && currentProject.description === description.textContent) {
+    //     title.textContent = currentProject.title;
+    //     description.textContent = currentProject.description;
+    // } else {
+        title.textContent = "";
+        description.textContent = "";
+        if (currentTodoButton != null) {
         currentTodoButton.remove();
     }
+    
+    // title.textContent = "";
+    // description.textContent = "";
+    // if (currentTodoButton != null) {
+    //     currentTodoButton.remove();
+    // }
+    // while (todosContainer.firstChild) {
+    //     todosContainer.removeChild(todosContainer.firstChild);
+    // }
 };
